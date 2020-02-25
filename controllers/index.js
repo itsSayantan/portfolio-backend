e1d86e004b63031d1a5ca0821dbc3fa1b7799e66
itsSayantan <sayantan.ghosh03@gmail.com>
2020-02-25 14:24:23 +0530

    1. Added the getTimeLineItems API.

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const fetch = require("node-fetch");
 
-const getAllProjects = (req, res, next) => {
-  const headers = {
-    Authorization: "Basic " + process.env.GITHUB_AUTH_STRING
-  };
+const headers = {
+  Authorization: "Basic " + process.env.GITHUB_AUTH_STRING
+};
 
+const getAllProjects = (req, res, next) => {
   fetch(
     process.env.GITHUB_API_ENDPOINT + "/users/itsSayantan/repos?sort=updated",
     {
@@ -36,6 +36,47 @@ const getAllProjects = (req, res, next) => {
     });
 };
 
+const getTimeLineItems = (req, res, next) => {
+  fetch(
+    process.env.GITHUB_API_ENDPOINT + "/repos/itsSayantan/portfolio-posts/contents/meta/posts.json",
+    {
+      headers
+    }
+  )
+    .then(response => {
+      if (response.status === 403) {
+        return {
+          status: 403,
+          responseBody: {
+            status: false,
+            message:
+              "You are forbidden to perform this action. This might be due to the rate limiting in Github APIs. Please try again after some time."
+          }
+        };
+      }
+      return response.json();
+    })
+    .then(jsonData => {
+      if (jsonData.status) {
+        res.status(jsonData.status).send(jsonData.responseBody);
+      } else {
+        if (jsonData && jsonData.content) {
+          const base64Content = jsonData.content
+          const stringContent = (Buffer.from(base64Content, 'base64')).toString('ascii')
+          const jsonParsedContent = JSON.parse(stringContent)
+
+          res.status(200).send(jsonParsedContent.timeLineItems || []);
+        } else {
+          res.status(500).send({ status: false, message: 'Invalid response from the Github API.' })
+        }
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
 module.exports = {
-  getAllProjects
+  getAllProjects,
+  getTimeLineItems
 };
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 const services = require("./controllers");
 
 app.get("/api/projects/getAll", services.getAllProjects);
+app.get("/api/getTimeLineItems", services.getTimeLineItems);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
