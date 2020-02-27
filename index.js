3c5a0fa891f246a53faca146705a179b6430390f
itsSayantan <sayantan.ghosh03@gmail.com>
2020-02-27 09:10:23 +0530

    1. Possible fix of API not getting called properly

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const root = __dirname + "/public";
 
 app.use(cors());
 
-app.use(express.static(root));
-app.use(fallback("index.html", { root: root }));
-
 const port = process.env.PORT || 5000;
 
 const services = require("./controllers");
@@ -18,6 +15,9 @@ app.get("/api/projects/getAll", services.getAllProjects);
 app.get("/api/getTimeLineItems", services.getTimeLineItems);
 app.get("/api/getGithubApiRateLimit", services.getGithubApiRateLimit);
 app.get("/api/posts/get/:id", services.getPostByID);
+
+app.use(express.static(root));
+app.use(fallback("index.html", { root: root }));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
