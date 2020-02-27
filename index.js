ef989b1c2abf1c94d8f45a6a66b55d2d1acc61f5
itsSayantan <sayantan.ghosh03@gmail.com>
2020-02-27 09:04:22 +0530

    1. UI build

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const path = require("path");
 const express = require("express");
+const fallback = require("express-history-api-fallback");
 const app = express();
 const cors = require("cors");
+const root = __dirname + "/public";
 
 app.use(cors());
+
+app.use(express.static(root));
+app.use(fallback("index.html", { root: root }));
 
 const port = process.env.PORT || 5000;
 
@@ -11,7 +17,7 @@ const services = require("./controllers");
 app.get("/api/projects/getAll", services.getAllProjects);
 app.get("/api/getTimeLineItems", services.getTimeLineItems);
 app.get("/api/getGithubApiRateLimit", services.getGithubApiRateLimit);
-app.get("/api/posts/get/:id", services.getPostByID)
+app.get("/api/posts/get/:id", services.getPostByID);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
